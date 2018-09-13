c0694fd363346679e44fa4defc8f3bd546e8f5e6
liuyang <250935102@qq.com>
2018-09-13 12:01:02 +0800

    刘洋

--- Java/src/Lambda/TestPredicate/EmptyFilterStringList.java
+++ Java/src/Lambda/TestPredicate/EmptyFilterStringList.java
@@ -8,26 +8,47 @@ import java.util.function.Predicate;
 /**
  * Created by Ly on 2018/9/11.
  */
-public class EmptyFilterStringList {
+public class FilterStringList {
     public static void main(String args[]) {
-        List<String> listS = Arrays.asList("1", "A", "B", "asdf", "", "12");
-        Predicate<String> predicate = (String s) -> !s.isEmpty();
-        List<String> result = filter(listS,predicate);
+        List<String> listS = Arrays.asList("134dff", "Asadf32", "B", "aliuyang", "", "12");
+        List<String> result = filter(listS, FilterStringList::testEmpty);
+        List<String> resultlength = filter(listS, FilterStringList::testLength);
+        List<String> resultliu = filter(listS, FilterStringList::testConLiu);
         log(result);
+        log(resultlength);
+        log(resultliu);
     }
 
+    public static boolean testEmpty(String test) {
+        return test.isEmpty();
+    }
+
+    public static boolean testLength(String test) {
+        return test.length() > 3;
+    }
+
+    public static boolean testConLiu(String test) {
+        return test.contains("liu");
+    }
+
+    /**
+     * @param stringList
+     * @param predicate
+     * @return
+     */
     public static List<String> filter(List<String> stringList, Predicate<String> predicate) {
         List<String> resultList = new ArrayList<String>();
         for (String str : stringList) {
-            if (!str.isEmpty()) {
+            if (!predicate.test(str)) {
                 resultList.add(str);
             }
         }
         return resultList;
     }
-    public  static void log(List<String> stringList){
-        for(String s:stringList){
-            System.out.println("SystemOutLine:"+s);
+
+    public static void log(List<String> stringList) {
+        for (String s : stringList) {
+            System.out.println("SystemOutLine:" + s);
         }
     }
 }
