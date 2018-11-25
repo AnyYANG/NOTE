022a1aaafed8e53d239b8673aa6c7c0ea7298b63
liuyang <250935102@qq.com>
2018-11-25 22:09:21 +0800

    Lambda var
    Signed-off-by: liuyang <250935102@qq.com>

---
+++ Java/src/Lambda/Variable/HeapandStack.java
@@ -0,0 +1,17 @@
+package Lambda.Variable;
+
+/**
+ * Created by Ly on 2018/11/25.
+ * 栈和堆 局部变量都是保存在栈上的。 当方法被执行完的时候 ，空间就被释放掉了。 所以  i的值没有被改变成2
+ */
+public class HeapandStack {
+    public static void main(String args[]) {
+        int i = 1;
+        show(i);
+        System.out.println(i);
+    }
+
+    private static void show(int x) {
+        x = 2;
+    }
+}
---
+++ Java/src/Lambda/Variable/Variable.java
@@ -0,0 +1,32 @@
+package Lambda.Variable;
+
+/**
+ * Created by Ly on 2018/11/25.
+ * Lambda 为什么要使用final类型的局部变量
+ *  1 实例变量在堆stack中，局部变量在栈heap上。
+ *  2 栈上的东西，用完就消失，new创建出来的东西都在堆上。用完之后靠垃圾回收机制不定期清除。
+ *  3
+ *
+ */
+public class Variable {
+    private static int i = 1;
+
+    public static void main(String args[]) {
+        final int j = 1;
+        int k = 1;
+        System.out.println(i);
+        Runnable run1 = () -> System.out.println(i++);
+        //Lambda中使用的局部变量必须是final的 不能更改局部变量的值
+        Runnable run2 = () -> System.out.println(j);
+        Runnable run3 = () -> System.out.println(k);
+        //这一句是错误示例。
+        //Runnable run4 = () -> System.out.println(k++);
+        // 变量k一旦被lambda所使用即被默认成了final类型。不能改变。无论是之前改变还是之后的改变。
+        // k++;
+        run1.run();
+        run2.run();
+        run3.run();
+        i = 100;
+        System.out.println(i);
+    }
+}
