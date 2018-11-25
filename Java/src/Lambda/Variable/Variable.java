9194f4e0ce276ca2875106ee47d9b94e974b2772
liuyang <250935102@qq.com>
2018-11-25 23:28:17 +0800

    liuyang
    局部变量

--- Java/src/Lambda/Variable/Variable.java
+++ Java/src/Lambda/Variable/Variable.java
@@ -5,7 +5,7 @@ package Lambda.Variable;
  * Lambda 为什么要使用final类型的局部变量
  *  1 实例变量在堆stack中，局部变量在栈heap上。
  *  2 栈上的东西，用完就消失，new创建出来的东西都在堆上。用完之后靠垃圾回收机制不定期清除。
- *  3
+ *  3 Lambda线程在访问局部变量的时候 是在访问局部变量的副本。如果副本被回收，或值发生改变Lambda访问的则不是原始变量。
  *
  */
 public class Variable {
