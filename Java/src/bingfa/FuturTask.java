6ab42abad973e6044fdcef77ffe8489a428f65de
liuyang <250935102@qq.com>
2018-12-11 17:56:36 +0800

    mysql variable article init

--- Java/src/bingfa/FuturTask.java
+++ Java/src/bingfa/FuturTask.java
@@ -4,8 +4,9 @@ package bingfa;
  * Created by  liuyang
  * 2018/12/6    20:10
  * bingfa
- * All Right Reserved by liuyang.
- **/
+
+        * All Right Reserved by liuyang.
+        **/
 
 public class FuturTask {
 
