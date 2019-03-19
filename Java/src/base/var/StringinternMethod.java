7c243516ee738084ff3c4dd7b2d4a62c8b29ef1b
刘洋 <250935102@QQ.COM>
2019-03-19 21:57:18 +0800

    String 常量池
    Signed-off-by: 刘洋 <250935102@QQ.COM>

--- Java/src/base/var/StringinternMethod.java
+++
@@ -1,9 +0,0 @@
-package base.var;
-
-/**
- *  liuyang  日期 2019年3月11日 22点14分
- *
- */
-public class StringinternMethod {
-
-}
---
+++ Java/src/test/java/base/var/StringinternMethodTest.java
@@ -0,0 +1,12 @@
+package base.var;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringinternMethodTest {
+
+    @Test
+    public void test1() {
+    }
+}
