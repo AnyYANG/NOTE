a3ad5a9e3aa37402eab6d01c8fd47b3a9fabb973
刘洋 <250935102@QQ.COM>
2019-03-19 22:27:20 +0800

    String 常量池
    Signed-off-by: 刘洋 <250935102@QQ.COM>

---
+++ Java/src/main/java/base/var/StringinternMethod.java
@@ -0,0 +1,17 @@
+package base.var;
+
+
+import org.junit.jupiter.api.Test;
+
+/**
+ *  liuyang  日期 2019年3月11日 22点14分
+ *   String 设计成常量的初衷d
+ */
+public class StringinternMethod {
+   @Test
+    public void test1(){
+        String a= "";
+        String b="";
+        System.out.println(a==b);
+    }
+}
