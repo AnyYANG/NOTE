8be2978582031633a3c783946b631435a88a3d55
liuyang <250935102@qq.com>
2019-03-21 16:36:33 +0800

    1
    Signed-off-by: liuyang <250935102@qq.com>

--- Java/src/bingfa/reeentrantlock/ReentrantlockTest.java
+++ Java/src/bingfa/reeentrantlock/ReentrantlockTest.java
@@ -1,6 +1,8 @@
 package bingfa.reeentrantlock;
 
 import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.locks.ReentrantLock;
 /**
  * Created by  liuyang
  * 2018/12/6    20:27
@@ -8,41 +10,15 @@ import org.junit.jupiter.api.Test;
  * All Right Reserved by liuyang.
  **/
 
-/**
+ /**
  * reentrantlock 被await后自动阻塞线程，并放弃锁。等待condition的signal
  */
 public class ReentrantlockTest {
     @Test
-    public void ReenLockawait() {
-      myrunable run =new myrunable();
-      run.run();
-        try {
-         //   RLock.lock.lock();
-            RLock.condition.signal();
-        } finally {
-            RLock.lock.unlock();
-        }
-    }
-
-    class myrunable implements  Runnable{
-
-        @Override
-        public void run() {
-            try {
-                RLock.lock.lock();
-                System.out.println("开始wait");
-                RLock.condition.await();  //放弃的锁，等待其他线程酱condition条件的唤醒
-                for (int i = 0; i < 5; i++) {
-                    //唤醒后执行的循环
-                    System.out.println("ThreadName=" + Thread.currentThread().getName()
-                            + (" " + (i + 1)));
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            } finally {
-                RLock.lock.unlock();
-            }
-        }
+    public void demo1(){
+        ReentrantLock reentrantLock=new ReentrantLock();
+        reentrantLock.lock();
+        reentrantLock.unlock();
     }
 }
 
